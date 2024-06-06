2b608e8f3f9a20e80efe7db4bc0717f85d3faa4b
Mateus Ribeiro <mateusbaaca@gmail.com>
2024-06-06 16:01:35 -0300

    Alterado o codigo para fazer o "Login" do usuario ser realizado por comandos da maneira que está nas especificações do projeto.

--- Client/index-client.py
+++ Client/index-client.py
@@ -2,11 +2,12 @@ import random
 import threading
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.bind(("localhost", random.randint(8000, 9000)))
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Inicialização do Cliente
+client.bind(("localhost", random.randint(8000, 9000))) 
 porta = 7777
-name = ""
+username = ""
 
+#Função que Recebe as mensagens
 def receive():
     while True:
         try:
@@ -18,15 +19,22 @@ def receive():
 thread1 = threading.Thread(target=receive)
 thread1.start()
 
+#Codigo que envia Mensagens
 while True:
-    message = input("")
-    if "toctoc" in message:
-        name = input("Usuário: ")
-        client.sendto(f"SIGNUP_TAG:{name}".encode(), ("localhost", 7777))
-    elif message == "bye":
+    message = input("") 
+
+    #Inicializa o usuario e sai caso o usuario digite "bye"
+    if message.startswith("hi, meu nome eh") or message.startswith("Hi, meu nome eh"):
+        username = message[len("hi meu nome eh") + 1:].strip()                                           #Função que separa o "hi meu nome eh do nome de usuario escrito"
+        client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{username}: hi, meu nome eh: {username}".encode(), ("localhost", 7777))                      #Envia ao servidor a mensagem de conexão inicial
+    elif username and message == "bye":
+        client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{username}:{message}".encode(), ("localhost", 7777))
+        print("Conexão encerrada, Até logo!")
         exit()
+    
+    #Envia a mensagem caso o usuario já esteja conectado    
     else:
-        if name:
-            client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{name}:{message}".encode(), ("localhost", 7777))
+        if username:
+            client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{username}:{message}".encode(), ("localhost", 7777))            #Envia a mensagem ao servidor
         else:
-            print("Você precisa fazer login com 'toctoc' antes de enviar mensagens.")
+            print("Para conectar a sala digite 'hi, meu nome eh' e digite seu username")
