1ec5c8e8a0fc11746caaab14af048feb64f9a9ab
Mateus Ribeiro <mateusbaaca@gmail.com>
2024-06-19 12:54:40 -0300

    teste

--- Client/index-client.py
+++ Client/index-client.py
@@ -1,40 +1,123 @@
 import random
 import threading
 import socket
+from datetime import datetime
+import os
+import math
+from zlib import crc32
+import struct
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Inicialização do Cliente
-client.bind(("localhost", random.randint(8000, 9000))) 
-porta = 7777
-username = ""
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind(("localhost", random.randint(8000, 9000)))
 
-#Função que Recebe as mensagens
+# Armazenamento de fragmentos recebidos
+frags_received_list = []
+frags_received_count = 0
+
+
+def unpack_and_reassemble(data):
+    global frags_received_count, frags_received_list
+
+    header = data[:16]
+    message_in_bytes = data[16:]
+    frag_size, frag_index, frags_numb, crc = struct.unpack('!IIII', header)
+
+    # Verificar CRC
+    if crc != crc32(message_in_bytes):
+        print("Fragmento com CRC inválido, ignorando.")
+        return
+
+    if len(frags_received_list) < frags_numb:
+        add = frags_numb - len(frags_received_list)
+        frags_received_list.extend([None] * add)
+
+    frags_received_list[frag_index] = message_in_bytes
+    frags_received_count += 1
+
+    if frags_received_count == frags_numb:
+        with open('received_message.txt', 'wb') as file:
+            for fragment in frags_received_list:
+                file.write(fragment)
+        frags_received_count = 0
+        frags_received_list = []
+        print_received_message()
+
+    elif (frags_received_count < frags_numb) and (frag_index == frags_numb - 1):
+        print("Provavelmente houve perda de pacotes")
+        frags_received_count = 0
+        frags_received_list = []
+
+
+def print_received_message():
+    with open('received_message.txt', 'r') as file:
+        file_content = file.read()
+    os.remove('received_message.txt')
+    print(file_content)
+
+
 def receive():
     while True:
-        try:
-            message, addr = client.recvfrom(1024)
-            print(message.decode())
-        except Exception as e:
-            print(f"Error receiving message: {e}")
+        data, addr = client.recvfrom(1024)
+        unpack_and_reassemble(data)
+
 
 thread1 = threading.Thread(target=receive)
 thread1.start()
 
-#Codigo que envia Mensagens
-while True:
-    message = input("") 
 
-    #Inicializa o usuario e sai caso o usuario digite "bye"
-    if message.startswith("hi, meu nome eh") or message.startswith("Hi, meu nome eh"):
-        username = message[len("hi meu nome eh") + 1:].strip()                                           #Função que separa o "hi meu nome eh do nome de usuario escrito"
-        client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{username}: hi, meu nome eh: {username}".encode(), ("localhost", 7777))                      #Envia ao servidor a mensagem de conexão inicial
-    elif username and message == "bye":
-        client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{username}:{message}".encode(), ("localhost", 7777))
-        print("Conexão encerrada, Até logo!")
-        exit()
-    
-    #Envia a mensagem caso o usuario já esteja conectado    
-    else:
-        if username:
-            client.sendto(f"<{client.getsockname()[0]}>:<{client.getsockname()[1]}>/~{username}:{message}".encode(), ("localhost", 7777))            #Envia a mensagem ao servidor
+def create_fragment(payload, frag_size, frag_index, frags_numb):
+    data = payload[:frag_size]
+    crc = crc32(data)
+    header = struct.pack('!IIII', frag_size, frag_index, frags_numb, crc)
+    return header + data
+
+
+def main():
+    username = ''
+
+    while True:
+        message = input("")
+
+        if message.startswith("hi, meu nome eh") or message.startswith("Hi, meu nome eh"):
+            username = message[len("hi, meu nome eh") + 1:].strip()
+            sent_msg = f"SIGNUP_TAG:{username}"
+            with open('message_client.txt', 'w') as file:
+                file.write(sent_msg)
+            send_txt()
+
+        elif username and message == "bye":
+            sent_msg = f"SIGNOUT_TAG:{username}"
+            with open('message_client.txt', 'w') as file:
+                file.write(sent_msg)
+            send_txt()
+            print("Conexão encerrada, Até logo!")
+            exit()
+
         else:
-            print("Para conectar a sala digite 'hi, meu nome eh' e digite seu username")
+            if username:
+                timestamp = datetime.now().strftime('%H:%M:%S - %d/%m/%Y')
+                formatted_message = f"{client.getsockname()[0]}:{client.getsockname()[1]}/~{username}: {message} {timestamp}"
+                with open('message_client.txt', 'w') as file:
+                    file.write(formatted_message)
+                send_txt()
+            else:
+                print("Para conectar a sala digite 'hi, meu nome eh' e digite seu username")
+
+
+def send_txt():
+    frag_index = 0
+    frag_size = 1024
+
+    with open('message_client.txt', 'rb') as file:
+        payload = file.read()
+        frags_numb = math.ceil(len(payload) / frag_size)
+
+        while payload:
+            fragment = create_fragment(payload, frag_size, frag_index, frags_numb)
+            client.sendto(fragment, ('localhost', 7777))
+            payload = payload[frag_size:]
+            frag_index += 1
+    os.remove('message_client.txt')
+
+
+main()
--- Server/index-server.py
+++ Server/index-server.py
@@ -1,43 +1,111 @@
-import threading
 import socket
+import os
 import queue
+import math
+import threading
+import struct
+from zlib import crc32
 
 clients = []
 messages = queue.Queue()
+
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(('localhost', 7777))
+
+frags_received_list = []
+frags_received_count = 0
+
+
+def create_fragment(payload, frag_size, frag_index, frags_numb):
+    data = payload[:frag_size]
+    crc = crc32(data)
+    header = struct.pack('!IIII', frag_size, frag_index, frags_numb, crc)
+    return header + data
+
+
+def unpack_and_reassemble(data, addr):
+    global frags_received_count, frags_received_list
+
+    header = data[:16]
+    message_in_bytes = data[16:]
+    frag_size, frag_index, frags_numb, crc = struct.unpack('!IIII', header)
+
+    # Verificar CRC
+    if crc != crc32(message_in_bytes):
+        print("Fragmento com CRC inválido, ignorando.")
+        return
+
+    if len(frags_received_list) < frags_numb:
+        add = frags_numb - len(frags_received_list)
+        frags_received_list.extend([None] * add)
+
+    frags_received_list[frag_index] = message_in_bytes
+    frags_received_count += 1
+
+    if frags_received_count == frags_numb:
+        with open('received_message.txt', 'wb') as file:
+            for fragment in frags_received_list:
+                file.write(fragment)
+        frags_received_count = 0
+        frags_received_list = []
+        process_received_message(addr)
+
+    elif (frags_received_count < frags_numb) and (frag_index == frags_numb - 1):
+        print("Provavelmente houve perda de pacotes")
+        frags_received_count = 0
+        frags_received_list = []
+
+
+def process_received_message(addr):
+    with open('received_message.txt', 'r') as file:
+        file_content = file.read()
+    os.remove('received_message.txt')
+    for line in file_content.strip().split('\n'):
+        line = line.strip()
+        if "SIGNUP_TAG:" in line:
+            name = line.split(":")[1]
+            sent_msg = f"{name} entrou na sala"
+            messages.put(sent_msg)
+        elif "SIGNOUT_TAG:" in line:
+            name = line.split(":")[1]
+            sent_msg = f"{name} saiu da sala"
+            messages.put(sent_msg)
+        else:
+            messages.put(line)
+    send_to_all_clients(addr)
+
+
+def send_to_all_clients(sender_addr):
+    frag_index = 0
+    frag_size = 1024
+    while not messages.empty():
+        message = messages.get()
+        with open('message_client.txt', 'w') as file:
+            file.write(message)
+
+        with open('message_client.txt', 'rb') as file:
+            payload = file.read()
+            frags_numb = math.ceil(len(payload) / frag_size)
+
+            for client in clients:
+                if client != sender_addr:  # Evitar enviar para o remetente original
+                    fragment_payload = payload
+                    fragment_index = 0
+                    while fragment_payload:
+                        fragment = create_fragment(fragment_payload, frag_size, fragment_index, frags_numb)
+                        server.sendto(fragment, client)
+                        fragment_payload = fragment_payload[frag_size:]
+                        fragment_index += 1
+        os.remove('message_client.txt')
 
 
 def receive():
     while True:
-        try:
-            message, addr = server.recvfrom(1024)
-            print(f"Received message from {addr}: {message.decode()}")
-            messages.put((message, addr))
-        except Exception as e:
-            print(f"Error receiving message: {e}")
+        data, addr = server.recvfrom(1024)
+        if addr not in clients:
+            clients.append(addr)
+        unpack_and_reassemble(data, addr)
 
 
-def broadcast():
-    while True:
-        while not messages.empty():
-            message, addr = messages.get()
-            if addr not in clients:
-                clients.append(addr)
-
-            for client in clients:
-                try:
-                    if message.decode().startswith("SIGNUP_TAG:"):
-                        name = message.decode().split(":")[1]
-                        server.sendto(f"{name} entrou".encode(), client)
-                    else:
-                        server.sendto(message, client)
-                except Exception as e:
-                    print(f"Error sending message to client {client}: {e}")
-                    clients.remove(client)
-
-
-thread_1 = threading.Thread(target=receive)
-thread_2 = threading.Thread(target=broadcast)
-thread_1.start()
-thread_2.start()
+thread = threading.Thread(target=receive)
+thread.start()
